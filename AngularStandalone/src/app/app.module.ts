ab90a0c94338a2922196cdfad8fc6b1837fadd6a
takumura <takumura@outlook.com>
2022-12-30 18:05:33 +0900

    use test component from library

--- AngularStandalone/src/app/app.module.ts
+++ AngularStandalone/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { HomeComponent } from './home/home.component';
+import { TagComponent } from 'docupacker-angular-lib';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, FetchDataComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule, TagComponent, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
